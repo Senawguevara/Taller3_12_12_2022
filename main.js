08ad477f6dd93ac0c8b9288e32a260c2d970aa02
Formadcol <wguevarac85@gmail.com>
2022-12-14 12:32:02 -0500

    actividad-3

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 addEventListener(`DOMContentLoaded`,(e) =>{
-//1.	Realizar un programa que permita leer 2 números diferentes y nos diga cual es el mayor de los 2 números.
+//3.	Realizar un programa que pueda sumar dos números leídos por teclado y escribir el resultado.	
 
 let num1 = parseInt(prompt('Digite el primer Numero'));
 let num2 = parseInt(prompt('Digite el Segundo Numero'));
 console.log('El numero uno digitado es '+ num1);
 console.log('El numero dos digitado es '+ num2);
 
-    let resultado;
-    if (num1 > num2) {
-        resultado = num1;
-    } else if (num2 > num1) {
-        resultado = num2;
-    }
-console.log('El numero mayor entre dos numeros diferentes es '+ resultado);
+    let resultado = num1 +num2;
+    
+    
+console.log('La suma de los dos numeros es: '+ resultado);
 
-document.querySelector('body').innerHTML =`<H1>Ejercicio 01</H1><br><h2>Realizar un programa que permita leer 2 números diferentes y nos diga cual es el mayor de los 2 números.</h2>
-<h3>Verifica cual numero es mayor: el numero uno digitado es ${num1}, el numero dos digitado es; ${num2}, El mayor de los dos numeros es ${resultado}</h3>
+document.querySelector('body').innerHTML =`<H1>Ejercicio 03</H1><br><h2>Realizar un programa que pueda sumar dos números leídos por teclado y escribir el resultado.</h2>
+<h3>El numero uno digitado es ${num1}, el numero dos digitado es; ${num2}, La suma de los dos numeros es:  ${resultado}</h3>
 `;
 
 })
