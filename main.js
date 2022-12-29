c9dcae857d18523264c0379c9944d2973e8c33da
Senawguevara <wguevara90@misena.edu.co>
2022-12-29 00:53:43 -0500

    ejercicio 6

--- main.js
+++ main.js
@@ -1,78 +1,23 @@
 addEventListener(`DOMContentLoaded`,(e) =>{
-//5.	Realizar un programa que permita escribir los 100 primeros pares	
+//6.	Realizar un programa que lea N números, calcule y escriba la suma de los pares y el producto de los impares.	
 
-document.querySelector('body').innerHTML=`5.	Realizar un programa que permita escribir los 100 primeros pares<br>Favor abrir las herramientas de desarrollo para ver el resultado `;
-let n = 100;
-
-for (i = 1; i <= n; i++) {
-    if (i % 2 == 0) {
-       console.log(i+" "); 
-    }
-}
-
-
-
-// let resultado;
-
-
-// if ( a > b) {
-//    if (a > c) {
-//       console.log ('El primer numero es A ' + a );      
-//    }else {
-//       console.log('El tercer numero es mayor C ' + c);
-//    }   
-// }if (b > c) {
-//    console.log('El numero mayor es B ' + b);
-   
-// } else{
-//    console.log('El numeor mayor es el C '+ c);
-   
-// };
-
-// if ( a > b) {
-//    if (b > c) {
-//       console.log ('El primer numero es C ' + c );      
-//    }elsec{
-//       console.log('El tercer numero es mayor B ' + b);
-//    }   
-// }if (a > c) {
-//    console.log('El numero mayor es C ' + c);
-   
-// } else {
-//    console.log('El numeor mayor es el A '+ a);   
-// }
-
-
-
-
-
-
-// if (A == B && A == B){
-//     console.log("Numero 1, Numero 2 y Numero 3 son iguales! y valen: "+A+".");
-//     } else {
-//        if (A > B){
-//           if (A > C){
-//              console.log("Numero 1 es Mayor y vale: "+ A+".");
-//           }else{
-//                 console.log("Numero 3 es Mayor y vale: "+ C +".");
-//           }
-//        } else{
-//           console.log("Numero 2 es Mayor y vale: "+ B +".");
-//        } 
-//       } 
-      
-      
-         
-         
-
-
-
+alert(`6.	Realizar un programa que lea N números, calcule y escriba la suma de los pares y el producto de los impares.<br>Favor abrir las herramientas de desarrollo para ver el resultado `);
 
     
-// console.log('La suma de los dos numeros es: '+ resultado);
+    let suma = 0;
+    let mul = 1; 
+    let num= Number(prompt("cuantos numeros ingresaras")); 
+    for (i=0; i < num; i++) {
+        let n= Number(prompt("Dame un numero")); 
+        if (n % 2 == 0) {
+            suma=suma+n;
+            
+        }else   {
+            mul=mul*n;
+        }
+    
+    }
 
-// document.querySelector('body').innerHTML =`<H1>Ejercicio 04</H1><br><h2>4.	Realizar un programa de procesos en el que se almacenen 3 números en 3 variables A, B y C. El diagrama debe decidir cual es el mayor y cual es el menor.</h2>
-// <h3>El numero uno digitado es $ A}, el numero dos digitado es; ${B}, La suma de los dos numeros es:  ${resultado}</h3>
-// `;
-
-})
+    console.log("la suma de los pares es: "+suma);
+    console.log("la multiplicacion de los impares es: "+mul);
+    })
